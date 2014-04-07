e2510a9b9e372232725810a83af307e306504997
Tusa Viktor <tusavik@gmail.com>
2014-04-07 22:25:28 +0200

    license: Added GPL license header to all code files.

--- scripts/phxcontrol.py
+++ scripts/phxcontrol.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+#
+# Copyright (c) 2008-2014 Viktor Tusa
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+#
+
 import os,sys,socket, struct, re
 import gtk
 from optparse import OptionParser
@@ -32,8 +51,6 @@ class Config:
         new_table = self.apptable.merge_changes(self.change_store)
         debug("new_table length: %d" % len(new_table.apptable))
         new_table.populate(self.liststore)
-
-                
 
 class Rule:
     def __init__(self, appname = "", pid = 0, direction = 0, verdict = 0, src_zone_id = 0, source_zone = "*", dst_zone_id = 0, dest_zone = "*"):
